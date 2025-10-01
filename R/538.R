e80388b2fb58a8624676b683c4931c372b1cf8ce
Christopher T. Kenny <ctkenny@proton.me>
2025-10-01 15:10:02 +0000

    version 0.0.4

--- R/538.R
+++ R/538.R
@@ -11,19 +11,26 @@
 #' data(oregon)
 #'
 #' ggplot(oregon, aes(fill = ndv / (ndv + nrv))) +
-#'     geom_sf(size = 0) +
-#'     scale_fill_538(name = '') +
-#'     theme_map()
+#'   geom_sf(size = 0) +
+#'   scale_fill_538(name = '') +
+#'   theme_map()
 #' @concept colors
 #' @export
 scale_fill_538 <- function(...) {
-  ggplot2::binned_scale('fill', '538',
-                        palette = function(x) ggredist$fivethirtyeight,
-                        breaks = c(0, 0.35, 0.45, 0.55, 0.65, 1),
-                        limits = c(.25, .75),
-                        oob = scales::squish,
-                        guide = 'colourbar',
-                        ...
+  breaks <- c(0, 0.35, 0.45, 0.55, 0.65, 1)
+  limits <- c(0.25, 0.75)
+  ivals <- scales::rescale(breaks, from = limits)
+  ggplot2::binned_scale('fill',
+    palette = function(x) {
+      ggredist$fivethirtyeight[
+        findInterval(x, ivals, all.inside = TRUE, rightmost.closed = TRUE)
+      ]
+    },
+    breaks = breaks,
+    limits = limits,
+    oob = scales::squish,
+    guide = 'colourbar',
+    ...
   )
 }
 
@@ -31,13 +38,19 @@ scale_fill_538 <- function(...) {
 #' @concept colors
 #' @export
 scale_color_538 <- function(...) {
-  ggplot2::binned_scale('color', '538',
-                        palette = function(x) ggredist$fivethirtyeight,
-                        breaks = c(0, 0.35, 0.45, 0.55, 0.65, 1),
-                        limits = c(.25, .75),
-                        oob = scales::squish,
-                        guide = 'colourbar',
-                        ...
+  breaks <- c(0, 0.35, 0.45, 0.55, 0.65, 1)
+  limits <- c(0.25, 0.75)
+  ivals <- scales::rescale(breaks, from = limits)
+  ggplot2::binned_scale('color',
+    palette = function(x) {
+      ggredist$fivethirtyeight[
+        findInterval(x, ivals, all.inside = TRUE, rightmost.closed = TRUE)
+      ]
+    },
+    breaks = breaks,
+    limits = limits,
+    oob = scales::squish,
+    guide = 'colourbar',
+    ...
   )
 }
-
